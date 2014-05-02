fedf08aab222064ca257872281fdc7f4b12e36c3
Madie <madina9696@gmail.com>
2014-05-01 23:31:33 -0400

    AuthorityFigureCritter/TeacherCritter
    dummy classes

---
+++ AuthorityFigureCritter.java
@@ -0,0 +1,5 @@
+public class AuthorityFigureCritter extends Critter {
+    //A dummy class that inherits all of its methods from Critter,
+    //present only to comply with the specifications of the project
+    
+}
---
+++ TeacherCritter.java
@@ -0,0 +1,5 @@
+public class TeacherCritter extends AuthorityFigureCritter {
+
+    //this is a dummy class that was written in order to comply w/ specs
+
+}
