70b23503c84c0436b7d9fcad65726ecb3eb935b6
Madie <madina9696@gmail.com>
2014-05-01 23:57:19 -0400

    SparseGrid
    began working on class

---
+++ ExerciseSolutions/Pt5Ex1.java
@@ -0,0 +1,29 @@
+//IMPORTANT: THE CLASS NAME DOES NOT CORRESPOND TO FILE NAME
+/*
+  Specs:
+  create class SparseBoundedGrid that uses a "sparse array" implementation
+  Solution doesn't need to be a generic class, you may simply use
+  ***occupants**** of type Object.
+  
+  The sparse array is an array of Linked Lists!
+  Each linked list entry holds both a grid occupant and a column index
+  Each entry in the array list is a linked list or is null of that row is empty
+
+  From the two choices given, I chose to implement the 1st one (with the raw
+  list nodes)
+
+  For a grid with r rows and c columns, sparse array has length r and each
+  linked list has a maximum length c.
+
+ */
+import java.util.*;
+
+public class SparseBoundedGrid {
+
+    private Object occupant;
+    private int col;
+    private SparseGridNode next;
+
+
+
+}
