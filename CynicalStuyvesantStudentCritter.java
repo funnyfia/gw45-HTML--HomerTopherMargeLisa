c79d80d94632a0acdedea6d1ccdf4dc633f5cd78
Fia <sofia.hargil@gmail.com>
2014-05-02 02:11:34 -0400

    Added a friggin swagtastic comment block

--- CynicalStuyvesantStudentCritter.java
+++ CynicalStuyvesantStudentCritter.java
@@ -1,4 +1,16 @@
-/*A new type of Critter has appeared on the scene:
+/*HTML--HomerTopherMargeLisa
+APCS PD 9
+HW35 -- GridWorld, Part 4
+2014-05-02
+
+class CynicalStuyvesantStudentCritter
+
+BEACUASE: (P.S. I see what you did there with that youtube thing.)
+BECAUSE WE ARE EXACTLY THAT!!! We arrived at Stuy fresh-faced and eager, but soon realized that this institution was no middle school cake walk. After being spoiled by easy grades and soft teachers in previous years, we quickly had to adapt or DIE. Now, a few years older, we watch each incoming class and laugh at them. We laugh hard, and we laugh cynically. We laugh as we watch friends turn on each other. We laugh as we watch young, rosy cheeks devole into sallow grey ones. And we also reflect. We reflect on how Stuyvesant has taught us nothing but cynicism and the art of scraping by. So CynicalStuyvesantStudentCritter perfectly embodies the horrors of our lives. We sincerely hope you do not enjoy this.
+
+JK WE LOVE STUY!!!!! Seriously, this was all a joke. We hope you DO enjoy!
+
+A new type of Critter has appeared on the scene:
 *CynicalStuyvesantStudentCritter Specs:
 
 *The CynicalStuyvesantStudentCritter uses the inherited getActors() method,
@@ -19,6 +31,9 @@
 *removed from the grid. The makeMove() method will be similar to the 
 *ChamelonCritter's makeMove() method: it will set the location to the nearest
 *TeacherCritter or AuthorityFigureCritter.
+
+ERRATA:
+Just as Mr.Brown advised us, we added TeacherCritter, which this class called for interaction with. Not as hard as it looked!
 */
 import info.gridworld.actor.Actor;
 import info.gridworld.actor.ActorWorld;
