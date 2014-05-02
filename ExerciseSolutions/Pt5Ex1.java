68923836ece8867596e2141099658d14acc697a6
Madie <madina9696@gmail.com>
2014-05-02 00:33:51 -0400

    done some of the reqs as specified by Grid class
    ~will take short break~

--- ExerciseSolutions/Pt5Ex1.java
+++ ExerciseSolutions/Pt5Ex1.java
@@ -19,8 +19,8 @@
   
   Needs to have all methods in the Grid interface:
   E get(Location loc)
-  int getNumCols()
-  int getNumRows()
+  int getNumCols() done
+  int getNumRows() done
   ArrayList<Location> getOccupiedLocations()
   boolean isValid(Location loc)
   E remove (Location loc)
@@ -29,14 +29,32 @@
 import info.gridworld.grid.*;
 import java.util.*;
 
-public class SparseBoundedGrid extends AbstractGrid{
-
+public class SparseBoundedGrid<E> extends AbstractGrid<E>{
     
-
+    private SparseGridNode[] nodeArray;
+    int cols, rows; 
+    
     public SparseBoundedGrid(int r, int c) {
 	if (r <= 0 || c <= 0) 
-	    throw new IndexOutOfBoundsException();
-	
+	    throw new IndexOutOfBoundsException();	
+	rows = r; //length of nodeArray (which stores the Nodes)
+	cols = c; //MAX length of each LinkedList stored in nodeArray
+	nodeArray = new nodeArray[rows];
+    }
+    
+    public int getNumRows() {
+	return rows;
+    }
+
+    public int getNumCols() {
+	return cols;
+    }
+    
+    public boolean isValid(Location loc) {
+	/* in order to be valid, the location must have a position greater
+	   than or equal to 0 for both row and col and must be less than
+	   the total length
+	 */
     }
     
     
